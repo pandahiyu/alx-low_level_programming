57e437967caccc34436425f4c9e5abf78506e9a7
hiyab <hiyuhasiet@gmail.com>
2022-03-17 02:22:49 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
  */
 void print_alphabet(void)
 {
-char a = 'a';
-while (a <= 'z')
-{
-_putchar(a);
-a++;
-}
-_putchar('\n');
+  char a = 'a';
+  while (a <= 'z')
+  {
+    _putchar(a);
+    a++;
+  }
+  _putchar('\n');
 }

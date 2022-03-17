04d5153dd7feedb316eaf4eb601e859db7c391f9
hiyab <hiyuhasiet@gmail.com>
2022-03-17 23:05:25 +0300

    Update and rename 2-largest_number.c to 2-largest_number.c, main.h

--- 0x03-debugging/2-largest_number.c, main.h
+++ 0x03-debugging/2-largest_number.c, main.h
@@ -1,25 +1,29 @@
 #include "main.h"
+
 /**
- * * largest_number -returns the largest of 3 numbers
- * @: first integer
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 int largest;
+
 if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b >= a && b>= c)
+else if (b >= a && b >= c)
 {
-largest =b;
+largest = b;
 }
 else
 {
-largest =c;
+largest = c;
 }
-return (0);
+
+return (largest);
 }
